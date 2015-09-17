c99e3f0ac5dd1775a52b665554e689b386cdf7a4
Kendall Shaw <kshaw@kendallshaw.com>
2015-09-16 19:19:17 -0700

    Reorganized directorys structure.
    Added separate library and catalog plugin skeletons.

--- test/java/org/ditang/relaxng/defaults/TestValidation.java
+++
@@ -1,8 +0,0 @@
-package org.ditang.relaxng.defaults;
-
-import org.ditang.relaxng.ConstantsForTests;
-import org.junit.BeforeClass;
-
-public class TestValidation implements DefaultsConstants, ConstantsForTests {
-
-}
